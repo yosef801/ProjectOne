0d2d8bbe2e0792bc65fb688e7e3336bb26559416
yosef801 <yosef801801@gmail.com>
2022-08-06 23:30:34 +0300

    Site

---
+++ app.js
@@ -0,0 +1,20 @@
+let currentPageId = "home-page";
+
+function makeMenuLinkChangePage(linkId, pageToShow) {
+  const pageLinkElement = document.getElementById(linkId);
+
+  pageLinkElement.addEventListener("click", function () {
+    console.log("you clicked me");
+    document.getElementById(pageToShow).classList.remove("d-none");
+
+    document.getElementById(currentPageId).classList.add("d-none");
+
+    currentPageId = pageToShow;
+  });
+}
+
+makeMenuLinkChangePage("home-page-link", "home-page");
+makeMenuLinkChangePage("about-page-link", "about-page");
+makeMenuLinkChangePage("media-page-link", "media-page");
+makeMenuLinkChangePage("faq-page-link", "faq-page");
+makeMenuLinkChangePage("contact-page-link", "contact-page");
