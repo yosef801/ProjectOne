7ca6ca871fe41fdbb78fedb30cd45aabd318ac45
yosef801 <yosef801801@gmail.com>
2022-08-07 11:40:50 +0300

    update

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ function makeMenuLinkChangePage(linkId, pageToShow) {
 
   pageLinkElement.addEventListener("click", function () {
     console.log("you clicked me");
+    if (currentPageId == pageToShow) {
+      return 0;
+    }
     document.getElementById(pageToShow).classList.remove("d-none");
 
     document.getElementById(currentPageId).classList.add("d-none");
@@ -19,3 +22,38 @@ makeMenuLinkChangePage("media-page-link", "media-page");
 makeMenuLinkChangePage("faq-page-link", "faq-page");
 makeMenuLinkChangePage("contact-page-link", "contact-page");
 makeMenuLinkChangePage("suppliers-page-link", "suppliers-page");
+
+function mediaPMV(mediaID, mediaToShow) {
+  const mediaToShowElement = document.getElementById(mediaID);
+  mediaToShowElement.addEventListener("click", function () {
+    const pM = document.getElementById("divPhotos");
+    const pV = document.getElementById("divVideos");
+    console.log("test test");
+    console.log(mediaToShowElement.value);
+    if (mediaToShowElement.value == 1) {
+      pM.classList.remove("d-none");
+      pV.classList.add("d-none");
+    } else {
+      pM.classList.add("d-none");
+      pV.classList.remove("d-none");
+    }
+  });
+}
+mediaPMV("mediaPM", "divPhotos");
+mediaPMV("mediaPV", "divVideos");
+
+var faqcurrentID = "null";
+function faqfun(faqID, faqToShow) {
+  const faqPAGEtoSHOW = document.getElementById(faqID);
+  faqPAGEtoSHOW.addEventListener("click", function () {
+    console.log("FAQ");
+    document.getElementById(faqToShow).classList.remove("d-none");
+    if (faqcurrentID == faqToShow) {
+      document.getElementById(faqToShow).classList.add("d-none");
+    }
+    faqcurrentID = faqToShow;
+  });
+}
+faqfun("faqONE", "qONE");
+faqfun("faqTWO", "qTWO");
+faqfun("faqTHREE", "qTHREE");
